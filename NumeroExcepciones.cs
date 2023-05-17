b328e065e7b51cdbc1e1a866fbb41fbce3f9fc21
Adrii-10 <133094878+Adrii-10@users.noreply.github.com>
2023-05-17 11:42:57 +0000

    second commit

---
+++ NumeroExcepciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class NumeroExcepciones
+{
+    public static void Main(string[] args)
+    {
+        string x = Console.ReadLine();
+
+        try
+        {
+            Convert.ToInt32(x);
+            Console.WriteLine("Es un numero");
+        }
+        catch
+        {
+            Console.WriteLine("No es un numero");
+        }
+    }
+}
