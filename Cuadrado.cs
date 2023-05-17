0f95ed82ecf60ff241f2973dc4d757f37ea82c26
Adrii-10 <133094878+Adrii-10@users.noreply.github.com>
2023-05-17 11:27:48 +0000

    second commit

---
+++ Cuadrado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un número:");
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el ancho del cuadrado:");
+        int ancho = int.Parse(Console.ReadLine());
+
+        // Validar el ancho para asegurarnos de que sea mayor que 1
+        if (ancho < 1)
+        {
+            Console.WriteLine("El ancho debe ser mayor que 1.");
+            return;
+        }
+
+        // Imprimir el cuadrado
+        for (int i = 0; i < ancho; i++)
+        {
+            for (int j = 0; j < ancho; j++)
+            {
+                Console.Write(numero);
+            }
+            Console.WriteLine();
+        }
+    }
+}
